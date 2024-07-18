80b6c26abde788b25b012355be47f5705c7b4eb1
chaudhariaish <chaudhariaish1997@gmail.com>
2024-07-18 12:09:35 +0000

    Add build files

--- build/precache-manifest.3be77d956fdf260f949d0e5a7dfe2b0a.js
+++ build/precache-manifest.3be77d956fdf260f949d0e5a7dfe2b0a.js
@@ -24,20 +24,20 @@ self.__precacheManifest = [
     "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "a98ffb55d01944963fd03148701d261e",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/counter-app/precache-manifest.e1ba752919a6ddc624326217738599b6.js"
+  "/counter-app/precache-manifest.3be77d956fdf260f949d0e5a7dfe2b0a.js"
 );
 
 workbox.clientsClaim();
